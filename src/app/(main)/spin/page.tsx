1ca6678fca225a821bd935ea2ce01a788b01d905
lukedotpng <lukecwoodland@gmail.com>
2025-03-14 19:18:33 -0700

    updated formatting funcs used in metadata

--- src/app/(main)/spin/page.tsx
+++ src/app/(main)/spin/page.tsx
@@ -4,6 +4,12 @@ import { Metadata } from "next";
 import { SpinInfo, SpinTarget } from "@/types";
 import { CreateSpinQuery, GetSpinFromQuery } from "@/lib/SpinQueryUtils";
 import { Suspense } from "react";
+import {
+    DisguiseIDToDisplayText,
+    MethodIDToDisplayText,
+    MissionIDToDisplayText,
+    TargetIDToDisplayText,
+} from "@/lib/FormattingUtils";
 
 export async function generateMetadata({
     searchParams,
@@ -23,11 +29,11 @@ export async function generateMetadata({
 
     spinQuery = CreateSpinQuery(spin);
 
-    const title = NameIDToDisplayText(spin.mission) + " Spin";
+    const title = MissionIDToDisplayText(spin.mission) + " Spin";
 
     let description = "";
     (Object.keys(spin.info) as (keyof SpinInfo)[]).map((target: SpinTarget) => {
-        description += `${NameIDToDisplayText(target)}: ${MethodIDToDisplayText(spin.info[target]?.condition)} / ${DisguiseIDToDisplayText(spin.info[target]?.disguise)}\n`;
+        description += `${TargetIDToDisplayText(target)}: ${MethodIDToDisplayText(spin.info[target]?.condition)} / ${DisguiseIDToDisplayText(spin.info[target]?.disguise)}\n`;
     });
 
     return {
@@ -69,56 +75,4 @@ export default function Page() {
             <MainSection />
         </Suspense>
     );
-}
-
-function MethodIDToDisplayText(item: string | undefined) {
-    if (!item) {
-        return "Err No Condition";
-    }
-    let itemDisplayText = "";
-    // disguise ID example: paris-palace_staff
-    const words = item.split("_"); // ["palace", "staff"]
-
-    for (let word of words) {
-        if (word.toLowerCase() === "smg") {
-            word = "SMG";
-        }
-        itemDisplayText += word.charAt(0).toUpperCase() + word.slice(1) + " ";
-    }
-
-    return itemDisplayText.trim();
-}
-
-function DisguiseIDToDisplayText(disguise: string | undefined) {
-    if (!disguise) {
-        return "Err No Disguise";
-    }
-    let disguiseDisplayText = "";
-    const words = disguise.split("_"); // ["palace", "staff"]
-
-    for (const word of words) {
-        disguiseDisplayText +=
-            word.charAt(0).toUpperCase() + word.slice(1) + " ";
-    }
-
-    return disguiseDisplayText.trim();
-}
-
-function NameIDToDisplayText(target: string) {
-    let nameDisplayText = "";
-    const words = target.split("_");
-
-    for (let word of words) {
-        if (word.toLowerCase() === "ica") {
-            word = "ICA";
-        }
-        if (word.toLowerCase() === "of") {
-            nameDisplayText += word.toLowerCase() + " ";
-        } else {
-            nameDisplayText +=
-                word.charAt(0).toUpperCase() + word.slice(1) + " ";
-        }
-    }
-
-    return nameDisplayText.trim();
 }
