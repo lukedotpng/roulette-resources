5430cb3e979936dbb59f2fbf6f77e6b9f68a3e72
lukedotpng <lukecwoodland@gmail.com>
2025-05-12 13:02:51 -0700

    started new home page

---
+++ src/app/(main)/_components/RouletteSubstackInfo.tsx
@@ -0,0 +1,107 @@
+type SubstackResponseObject = {
+    id: number;
+    title: string;
+    post_date: string;
+    canonical_url: string;
+    reactions: {
+        "❤": number;
+    };
+    subtitle: string;
+    cover_image: string;
+    description: string;
+    truncated_body_text: string;
+    publishedBylines: [
+        {
+            id: number;
+            name: string;
+            handle: string;
+            photo_url: string;
+            bio: string;
+            profile_set_up_at: string;
+            reader_installed_at: string;
+            publicationUsers: [
+                {
+                    id: number;
+                    user_id: number;
+                    publication_id: number;
+                    role: string;
+                    public: boolean;
+                    is_primary: boolean;
+                    publication: {
+                        id: number;
+                        name: string;
+                        subdomain: string;
+                        hero_text: string;
+                        logo_url: string;
+                        theme_var_background_pop: string;
+                        email_from_name: string;
+                        copyright: string;
+                    };
+                },
+            ];
+        },
+    ];
+    reaction_count: number;
+    comment_count: number;
+    child_comment_count: number;
+};
+
+export default async function RouletteSubstackInfo() {
+    let hitmanrrSubstackRes: SubstackResponseObject[] = (await fetch(
+        "https://hitmanrr.substack.com/api/v1/archive?sort=new&search=&offset=0&limit=0",
+    ).then((res) => res.json())) as SubstackResponseObject[];
+
+    if (hitmanrrSubstackRes.length > 3) {
+        hitmanrrSubstackRes = hitmanrrSubstackRes.slice(0, 3);
+    }
+
+    return (
+        <ul className="md w-full rounded-xl border-white bg-[#ffecec] shadow-md shadow-black">
+            {hitmanrrSubstackRes.map((entry) => {
+                const date = new Date(entry.post_date);
+                const dateString =
+                    date.toDateString().split(" ")[1] +
+                    " " +
+                    date.toDateString().split(" ")[2];
+
+                return (
+                    <li
+                        key={entry.id}
+                        className={
+                            "items-between group flex flex-col justify-start gap-4 p-2 last:pb-0"
+                        }
+                    >
+                        <div className="flex h-16 items-center sm:h-20">
+                            <a
+                                href={entry.canonical_url}
+                                target="_blank"
+                                className="group m-auto flex h-full w-full flex-col"
+                            >
+                                <h2 className="text-[1em] font-semibold group-hover:underline">
+                                    {entry.title}
+                                </h2>
+                                <h3 className="text-[.9em]">
+                                    {entry.subtitle}
+                                </h3>
+                                <div className="flex-1"></div>
+                                <p className="text-[.7em] text-zinc-400">
+                                    <span>{dateString}</span>
+                                    <span>{" - "}</span>
+                                    <span>
+                                        {entry.publishedBylines[0].name}
+                                    </span>
+                                </p>
+                            </a>
+                            <img
+                                src={entry.cover_image}
+                                alt={"Article Cover Image"}
+                                className="aspect-square h-[95%] rounded-md object-cover"
+                            />
+                        </div>
+                        <div className="m-auto w-[95%] self-center border-t-1 border-zinc-500 group-last:border-0"></div>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,65 @@
+import Link from "next/link";
+import RouletteSubstackInfo from "./_components/RouletteSubstackInfo";
+
 export default function Home() {
     return (
-        <main className="flex flex-col items-center justify-center text-white">
-            <h1 className="p-20 text-center text-2xl font-bold sm:text-4xl md:text-5xl">
-                Roulette Resources
+        <main className="flex flex-col items-center justify-center gap-5 text-white">
+            <h1 className="pt-5 text-center text-[2.2em] font-bold">
+                {"Roulette Resources"}
             </h1>
+
+            <div className="mb-5 flex w-full flex-wrap justify-center gap-2 px-5 sm:gap-2 sm:px-10">
+                <div className="flex max-h-[30rem] w-full max-w-[40rem] flex-1 flex-col gap-2">
+                    <section className="flex h-fit min-w-72 items-center justify-center gap-4 rounded-xl border-4 border-zinc-500 bg-white p-2 text-zinc-900">
+                        <h2 className="text-[1.25em] font-bold italic">
+                            {"Bored?"}
+                        </h2>
+                        <Link
+                            href={"/spin"}
+                            target="_blank"
+                            className="rounded-md border-2 border-zinc-900 bg-white px-2 text-[1.2em] font-bold text-zinc-900 hover:border-red-500 hover:bg-red-500 hover:text-white"
+                        >
+                            {"Go Spin!"}
+                        </Link>
+                    </section>
+                    <section className="flex h-fit min-w-72 flex-1 flex-col items-center gap-5 rounded-xl border-4 border-zinc-500 bg-white p-2 text-zinc-900">
+                        <h2 className="text-[1.2em] font-bold">
+                            {"Citizens of Luke Town"}
+                        </h2>
+                        <ul className="w-64 flex-1 list-inside text-center text-[1.05em]">
+                            <li className="rounded-md py-1 shadow-[0_0px_10px] shadow-red-500">
+                                {"ThatObserver"}
+                            </li>
+                        </ul>
+                        <a
+                            href="https://ko-fi.com/lukedot"
+                            target="_blank"
+                            className="w-full text-center text-[.9em] italic underline"
+                        >
+                            {"Pay your taxes to support this site!"}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                                className="inline h-3.5 fill-zinc-900 pb-0.5 pl-1"
+                            >
+                                {/* Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2024 Fonticons, Inc. */}
+                                <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l82.7 0L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3l0 82.7c0 17.7 14.3 32 32 32s32-14.3 32-32l0-160c0-17.7-14.3-32-32-32L320 0zM80 32C35.8 32 0 67.8 0 112L0 432c0 44.2 35.8 80 80 80l320 0c44.2 0 80-35.8 80-80l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 112c0 8.8-7.2 16-16 16L80 448c-8.8 0-16-7.2-16-16l0-320c0-8.8 7.2-16 16-16l112 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L80 32z" />
+                            </svg>
+                        </a>
+                    </section>
+                </div>
+                <section className="flex max-h-[30rem] w-full max-w-[40rem] min-w-72 flex-col items-center gap-1 overflow-scroll rounded-xl border-4 border-zinc-500 bg-white p-2 text-zinc-900">
+                    <h1 className="text-[1.2em] font-bold">
+                        {"Read Roulette Articles by In4Fun!"}
+                    </h1>
+                    <h2 className="pb-1 text-[.9em]">
+                        {
+                            "Learn about past rivalries, the biggest what-ifs, and more!"
+                        }
+                    </h2>
+                    <RouletteSubstackInfo />
+                </section>
+            </div>
         </main>
     );
 }
