a4755678c036507d3fb0c35a87638cd006d27395
lukedotpng <lukecwoodland@gmail.com>
2025-06-05 16:29:49 -0700

    added aphro to citizens <3

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -36,6 +36,9 @@ export default function Home() {
                             <li className="li w-52 max-w-full rounded-md py-1 shadow-[0_0px_10px] shadow-red-500">
                                 {"SpottheCat"}
                             </li>
+                            <li className="li w-52 max-w-full rounded-md py-1 shadow-[0_0px_10px] shadow-red-500">
+                                {"Aphro"}
+                            </li>
                         </ul>
                         <div className="flex-1"></div>
                         <a
