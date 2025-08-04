9d44140af271266cd33124af568c1aae97fdcfcc
luke <hey@luke.town>
2025-08-04 11:23:44 -0700

    Shouldve removed when removing drizzle-kit

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-    dialect: "postgresql",
-    schema: "./src/server/db/schema.ts",
-    dbCredentials: {
-        url: process.env.DRIZZLE_KIT_DATABASE_URL!,
-    },
-});
