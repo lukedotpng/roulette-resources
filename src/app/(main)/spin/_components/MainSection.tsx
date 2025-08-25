268d62e25c9114e45660417e0c51e56ed4384b05
Luke Woodland <lukewoodland2003@gmail.com>
2025-08-25 05:20:06 -0700

    Fixed incorrect argument passed to SpinTips

--- src/app/(main)/spin/_components/MainSection.tsx
+++ src/app/(main)/spin/_components/MainSection.tsx
@@ -118,7 +118,7 @@ export default function MainSection() {
 
             {spinManager.options.showTips.value && spinManager.currentSpin && (
                 <SpinTipsSection
-                    query={spinManager.spinQuery}
+                    query={spinManager.spinQuery.query}
                     mission={spinManager.currentSpin.mission}
                     matchModeManager={spinManager.matchModeManager}
                 />
