8f5512e262c2f8670c2c66d257bdc02f3bdc5900
luke <hey@luke.town>
2025-11-01 15:12:32 -0700

    fix: lowered z-value, z-values might need a relooking cause i think i over use them :/

--- src/app/(main)/[mission]/_components/MissionPageNav.tsx
+++ src/app/(main)/[mission]/_components/MissionPageNav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function MissionPageNav() {
     return (
-        <nav className="sticky top-0 z-30 w-full max-w-full px-2 py-1 text-[.9em] text-zinc-900 sm:w-fit sm:px-5 sm:py-2 sm:text-[1em]">
+        <nav className="sticky top-0 z-10 w-full max-w-full px-2 py-1 text-[.9em] text-zinc-900 sm:w-fit sm:px-5 sm:py-2 sm:text-[1em]">
             <div className="flex w-full flex-wrap justify-center gap-2 rounded-xl border-2 border-zinc-500 bg-white p-1 shadow-lg shadow-black sm:gap-5 sm:border-4">
                 <NavLink target="#timings">{"Timings"}</NavLink>
                 <NavLink target="#items">{"Items"}</NavLink>
