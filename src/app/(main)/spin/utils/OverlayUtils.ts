d005348c0d4232404827e458b72a6048ce8f7b86
luke <hey@luke.town>
2026-01-07 13:09:23 -0800

    refactor(overlay): removed unused log and unnecessary variable

--- src/app/(main)/spin/utils/OverlayUtils.ts
+++ src/app/(main)/spin/utils/OverlayUtils.ts
@@ -3,16 +3,14 @@ import { OverlayUpdateRequest } from "../types";
 export async function UpdateOverlay(
     requestBody: OverlayUpdateRequest,
 ): Promise<Response> {
-    const overlayUpdateBody: Omit<OverlayUpdateRequest, "id"> = requestBody;
-
     console.log("requestBody:", requestBody);
-    console.log("overlayUpdateBody:", overlayUpdateBody);
+
     const res = await fetch(
         "https://rouletteoverlay.luke.town/" + requestBody.id + "/update",
         {
             mode: "no-cors",
             method: "POST",
-            body: JSON.stringify(overlayUpdateBody),
+            body: JSON.stringify(requestBody),
         },
     );
 
